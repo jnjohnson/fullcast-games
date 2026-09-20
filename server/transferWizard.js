8c1f4e9260b6dad35b48a1b8ca0c2cadab696bd1
Jimmy Johnson <jimmy@jnjohnson.io>
2026-09-20 12:52:45 -0500

    Future todo for an edge case I found

--- server/transferWizard.js
+++ server/transferWizard.js
@@ -3,6 +3,8 @@ import { kvGet, kvPut } from './kv.js';
 const p4_schools = ["Alabama","Arizona","Arizona State","Arkansas","Auburn","Baylor","Boston College","BYU","California","Cincinnati","Clemson","Colorado","Duke","Florida","Florida State","Georgia","Georgia Tech","Houston","Illinois","Indiana","Iowa","Iowa State","Kansas","Kansas State","Kentucky","Louisville","LSU","Maryland","Miami","Michigan","Michigan State","Minnesota","Mississippi State","Missouri","NC State","Nebraska","North Carolina","Northwestern","Ohio State","Oklahoma","Oklahoma State","Ole Miss","Oregon","Penn State","Pittsburgh","Purdue","Rutgers","SMU","South Carolina","Stanford","Syracuse","TCU","Tennessee","Texas","Texas A&M","Texas Tech","UCF","UCLA","USC","Utah","Vanderbilt","Virginia","Virginia Tech","Wake Forest","Washington","West Virginia","Wisconsin"];
 const p4_conferences = ["ACC", "Big Ten", "Big 12", "SEC"];
 
+// TODO: Isaiah Johnson (CB) has a crazy looking transfer history. Investigate what's happening there.
+
 // Returns an array of size [size] of elements from the input array
 function getRandIndicies(arr, size) {
     var randArr = [];
